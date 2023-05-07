415c9fdbff089c93a22ad96168744ccd781fdefb
Nguyen Trung Kien <kiennt0x@gmail.com>
2023-05-08 00:10:21 +0700

    .

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,7 +6,8 @@ const password: string | undefined = process.env.DB_PASSWORD ?? "";
 const database: string | undefined = process.env.DB_DATABASE ?? '';
 const sequelizeConnection = new Sequelize(database, user, password, {
   host: host,
-  dialect: 'mysql',
+  dialect: "mysql",
   dialectModule: mysql2,
+  ssl: false,
 });
 export default sequelizeConnection;
