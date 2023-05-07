bbedd0c938c2b9aad2ecdbb4999ed78ae981452b
Nguyen Trung Kien <kiennt0x@gmail.com>
2023-05-07 20:51:45 +0700

    .

--- src/core/logger/logger.ts
+++ src/core/logger/logger.ts
@@ -1,15 +1,15 @@
-// import winston from 'winston'
-// import { v4 } from 'uuid'
+import winston from 'winston'
+import { v4 } from 'uuid'
 
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   defaultMeta: { service: 'server', correlationId: v4() },
-//   transports: [
-//     new winston.transports.Console({
-//       format: winston.format.simple()
-//     })
-//   ]
-// })
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'server', correlationId: v4() },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  ]
+})
 
-// export default logger
+export default logger
